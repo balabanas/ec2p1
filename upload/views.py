ad6638583a4ad4dd38727338664c59c4cda544d3
Anton Balabanov <asbbnv@gmail.com>
2023-02-23 16:45:19 +0200

    Init

---
+++ upload/views.py
@@ -0,0 +1,32 @@
+import os
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render
+
+from .models import Upload
+
+
+def image_upload(request):
+    print('Im at the top of image_upload view')
+    if request.method == 'POST':
+        print('before setting file image, file image type')
+        image_file = request.FILES['image_file']
+        print(dir(request))
+        image_type = request.POST['image_type']
+        print('before checking if settings USE3')
+        print('settings.USE_S3', settings.USE_S3)
+        if settings.USE_S3:
+            print('I am gere (before creating image instance)')
+            upload = Upload(file=image_file)
+            upload.save()
+            image_url = upload.file.url
+            print('image_url', image_url)
+        else:
+            fs = FileSystemStorage()
+            filename = fs.save(image_file.name, image_file)
+            image_url = fs.url(filename)
+        return render(request, 'upload/upload.html', {
+            'image_url': image_url
+        })
+    return render(request, 'upload/upload.html', {'debug': settings.DEBUG})
